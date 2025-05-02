5e01a789c61dcf0fdcecce7b544779f18af4e475
krejdlt22 <krejdlt22@spstrutnov.cz>
2025-05-02 12:12:26 +0200

    dokončení registrace a přihlášení

---
+++ public/script.js
@@ -0,0 +1,31 @@
+document.getElementById('registerForm').addEventListener('submit', async e => {
+    e.preventDefault();
+    const username = document.getElementById('regUsername').value;
+    const password = document.getElementById('regPassword').value;
+    const agreed = document.getElementById('agreeCheckbox').checked;
+  
+    const res = await fetch('/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password, agreed })
+    });
+  
+    const data = await res.json();
+    document.getElementById('message').textContent = data.message;
+  });
+  
+  document.getElementById('loginForm').addEventListener('submit', async e => {
+    e.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+  
+    const res = await fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+    });
+  
+    const data = await res.json();
+    document.getElementById('message').textContent = data.message;
+  });
+  
